95ac58602a10fbb8cfbcf5810f0f3c9f326dfb3d
Prav1t <pravit.hundal05@gmail.com>
2025-12-25 18:12:14 -0800

    Created Legend.jsx

---
+++ src/Legend.jsx
@@ -0,0 +1,11 @@
+export default function Legend() {
+  return (
+    <div className="legend">
+      <h4>Wildfire Risk</h4>
+      <div><span className="low" /> Low</div>
+      <div><span className="moderate" /> Moderate</div>
+      <div><span className="high" /> High</div>
+      <div><span className="extreme" /> Extreme</div>
+    </div>
+  );
+}
